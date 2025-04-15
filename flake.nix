285976a15d2a6b6af9a3de8fa7d989e973d30162
Mathewos Samson <mathos.brook@gmail.com>
2025-04-15 14:36:13 -0400

    basic modularity attempt

--- flake.nix
+++ flake.nix
@@ -1,20 +1,29 @@
 {
-  description = "A simple NixOS flake";
+  description = "NixOS config for my computers :3c";
 
   inputs = {
     # NixOS official package source, using the nixos-24.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    # Please replace nixos with your hostname
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./configuration.nix
+
+    nixosConfigurations = {
+      virtualbox = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+        ./virtualbox/configuration.nix
+        ./modules/common.nix
       ];
+      };
+      laptop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+        ./laptop/configuration.nix
+        ./modules/common.nix
+      ];
+      };
     };
   };
 }
