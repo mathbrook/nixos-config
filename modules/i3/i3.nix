653802682f6420a4932ddd19abfa6c00e5c618f7
Matthew Samson <mathos.brook@gmail.com>
2025-04-16 04:41:52 -0400

    broken workspaces, but looks nice

--- modules/i3/i3.nix
+++ modules/i3/i3.nix
@@ -10,15 +10,15 @@
   home.file.".config/i3/config" = { source = ./config; force = true; };
   home.file.".config/picom/picom.conf" = { source = ./picom.conf; force = true; };
   home.file.".config/libinput-gestures/libinput-gestures.conf" = { source = ./libinput-gestures.conf; force = true; };
-
-  # home.file.".config/i3/i3blocks.conf".source = ./i3blocks.conf;
-  # home.file.".config/i3/keybindings".source = ./keybindings;
-  # home.file.".config/i3/scripts" = {
-  #   source = ./scripts;
-  #   # copy the scripts directory recursively
-  #   recursive = true;
-  #   executable = true;  # make all scripts executable
-  # };
+  # New shti below
+  home.file.".config/i3/i3blocks.conf".source = ./i3blocks.conf;
+  home.file.".config/i3/keybindings".source = ./keybindings;
+  home.file.".config/i3/scripts" = {
+    source = ./scripts;
+    # copy the scripts directory recursively
+    recursive = true;
+    executable = true;  # make all scripts executable
+  };
 
 
   # set cursor size and dpi for 4k monitor
